4d3db73338c9e443a5e58a7df5745785bc2e39cb
Shamil Mollachiev <mollachievshamil@yandex.ru>
2022-03-22 09:12:32 +0700

    Initial Commit

---
+++ Random Photoes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photoes App
+//
+//  Created by Шамиль Моллачиев on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
